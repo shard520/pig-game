e89fcc6d0ed9479649fdc9098153445812aaa5e6
shard520 <65344196+shard520@users.noreply.github.com>
2021-03-05 14:51:32 +0100

    Add dice roll functionality - update dice image and currentScore on each dice roll.

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Game variables
+let currentScore = 0;
+
+// Start game
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// Roll dice
+btnRoll.addEventListener('click', function () {
+  // Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice roll
+  diceEl.classList.remove('hidden');
+  diceEl.src = `./img/dice-${dice}.png`;
+  // Check for rolled 1: if true switch player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch player
+  }
+});
