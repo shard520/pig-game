2a41722a2c6a0bdfc9b59af78c1a67d7490f07bb
shard520 <65344196+shard520@users.noreply.github.com>
2021-03-05 22:18:50 +0100

    Add game reset feature: move all game starting conditions to init function, call init on page load and when newBtn is clicked. Declare game variables outside of init with no values and assign in function.

--- script.js
+++ script.js
@@ -3,31 +3,53 @@
 // Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Game variables
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
-// Start game
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// Initialise game
+const init = function () {
+  // Game variables
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  // return scores, currentScore, activePlayer, playing;
+};
+
+init();
 
 // Change active player
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
+
   activePlayer = activePlayer === 0 ? 1 : 0;
+
   currentScore = 0;
+
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
@@ -37,13 +59,16 @@ btnRoll.addEventListener('click', function () {
   if (playing) {
     // Generate random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
+
     // Display dice roll
     diceEl.classList.remove('hidden');
     diceEl.src = `./img/dice-${dice}.png`;
+
     // Check for rolled 1: if true switch player
     if (dice !== 1) {
       // Add dice to current score
       currentScore += dice;
+
       document.getElementById(
         `current--${activePlayer}`
       ).textContent = currentScore;
@@ -59,13 +84,16 @@ btnHold.addEventListener('click', function () {
   if (playing) {
     // Add current score to score of active player
     scores[activePlayer] += currentScore;
+
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // Check if player's score >= 100
     if (scores[activePlayer] >= 100) {
       // Finish game
       playing = false;
+
       diceEl.classList.add('hidden');
+
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -78,3 +106,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// New game
+btnNew.addEventListener('click', init);
