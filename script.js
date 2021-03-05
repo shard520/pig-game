aca0656df1222bc4ac1ba284bf0c75fd28354d16
shard520 <65344196+shard520@users.noreply.github.com>
2021-03-05 21:18:46 +0100

    Implement hold score feature: when player holds score their current score is updated in the scores array and score element, if player's score >=100 dice image is removed and player--winner class is added. Playing boolean variable added initially set to true, code inside event listeners for roll and hold buttons moved into an if statement that checks if playing is true, when a player wins the game playing is set to false. Switch player code block moved to function which is called inside roll and hold event listeners.

--- script.js
+++ script.js
@@ -16,33 +16,65 @@ const btnHold = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 // Start game
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+// Change active player
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Roll dice
 btnRoll.addEventListener('click', function () {
-  // Generate random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // Display dice roll
-  diceEl.classList.remove('hidden');
-  diceEl.src = `./img/dice-${dice}.png`;
-  // Check for rolled 1: if true switch player
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    document.getElementById(
-      `current--${activePlayer}`
-    ).textContent = currentScore;
-  } else {
-    // Switch player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+  if (playing) {
+    // Generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // Display dice roll
+    diceEl.classList.remove('hidden');
+    diceEl.src = `./img/dice-${dice}.png`;
+    // Check for rolled 1: if true switch player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(
+        `current--${activePlayer}`
+      ).textContent = currentScore;
+    } else {
+      // Switch player
+      switchPlayer();
+    }
   }
 });
 
+// Hold score
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Add current score to score of active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // Check if player's score >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch player
+      switchPlayer();
+    }
+  }
+});
